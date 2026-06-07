3260a2a0588d44919a117f8da9adf7721a0e5806
SNJ2009 <nagjun2009@gmail.com>
2026-06-08 08:18:40 +0900

    .

--- src/main/java/org/ss/db/DBUtil.java
+++ src/main/java/org/ss/db/DBUtil.java
@@ -25,16 +25,15 @@ public class DBUtil {
 
     @SneakyThrows
     public static <T> T executeQueryForObject(String sql, RowMapper<T> rm, Object... params) { // 하나만 조회
-        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+        try (PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
 
             for (int i = 0; i < params.length; i++) {
                 ps.setObject(i + 1, params[i]);
             }
 
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    return rm.mapRow(rs);
-                }
+            if (rs.next()) {
+                return rm.mapRow(rs);
             }
         }
         return null;
@@ -44,19 +43,18 @@ public class DBUtil {
     public static <T> List<T> executeQueryForList(String sql, RowMapper<T> rm, Object... params) { // 여러 개 조회
         List<T> list = new ArrayList<>(); // 결과 담아둘 리스트 생성
 
-        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+        try (PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
 
             for (int i = 0; i < params.length; i++) {
                 ps.setObject(i + 1, params[i]);
             }
 
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    list.add(rm.mapRow(rs)); // 리스트에 담기
-                }
+            while (rs.next()) {
+                list.add(rm.mapRow(rs)); // 리스트에 담기
             }
         }
-        return list; // 데이터가 하나도 없으면 빈 리스트(.size() = 0)
+        return list;
     }
 
     // 데이터 조립용
